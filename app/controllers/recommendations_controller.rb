7adb4bc81e470cf945cf9e5d5b044ce69ad2fefb
Valentin Menard <valentin.menard@essec.edu>
2015-04-28 15:20:39 +0200

    routes finished

---
+++ app/controllers/recommendations_controller.rb
@@ -0,0 +1,11 @@
+class RecommendationsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  resources :users, only: [:show, :edit, :update]
+  resources :friendships, only: [:index, :new, :create, :destroy]
+
+  resources :restaurants, only: [:show, :index] do
+    resources :recommendations, only: [:new, :create]
+  end
+
+  resources :recommendations, only: :index
 end
