02cc899c82cd49375eef145933ff0304892ee23c
Valentin Menard <valentin.menard@essec.edu>
2015-04-28 14:51:43 +0200

    fin set up modèles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :pages_controller?
+  # on laisse unless pages_controller au cas ou pour l'instant
   include Pundit
 
   protect_from_forgery with: :exception
-
-  before_action :authenticate_user!, unless: :pages_controller?
 
   after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
