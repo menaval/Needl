8e973a302aa3bb128dcdc1e4c54ca30d270717ec
charlotte cady <charlotte.cady@hotmail.fr>
2015-04-28 17:50:57 +0200

    commencement du travail sur le controlleur recommendation - récupération des nouvelles routes

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,25 @@
 class RecommendationsController < ApplicationController
   def index
-
+    @recommendations = Recommendation.all
   end
 
   def new
+    @recommendation = Recommendation.new
 
   end
 
   def create
+    @recommendation = Recommendation.new(recommendation_params)
+    if @recommendation.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def recommendation_params
+    params.require(:recommendation).permit(:price, :review, :strengths, :ambiences)
+  end
 end
