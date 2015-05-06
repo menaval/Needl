5c79eb90db5dcf39599f079850509183099ba859
Valentin Menard <valentin.menard@essec.edu>
2015-05-06 10:43:08 +0200

    récupération des adresses et photos et création dans la table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503070828) do
+ActiveRecord::Schema.define(version: 20150506084102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,12 +98,14 @@ ActiveRecord::Schema.define(version: 20150503070828) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "food_id"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "price",      default: 0
+    t.integer  "price",        default: 0
+    t.string   "phone_number"
+    t.string   "picture_url"
   end
 
   add_index "restaurants", ["food_id"], name: "index_restaurants_on_food_id", using: :btree
