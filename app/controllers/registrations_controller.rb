0c2f2db0981d111d643c033856a05f66a47339ce
Valentin Menard <valentin.menard@essec.edu>
2015-05-16 16:01:45 +0200

    nada

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_up_path_for(resource)
-    new_friendship_path
-  end
-
-
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
