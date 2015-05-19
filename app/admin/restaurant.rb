32be6aec4cb27da24ed2c9a4540b8cc9de8df222
Valentin Menard <valentin.menard@essec.edu>
2015-05-19 17:30:27 +0200

    admin strong params

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Restaurant do
 
-  permit_params :name, :address, :food_id, :longitude, :latitude, :price
+  permit_params :name, :address, :food_id, :longitude, :latitude, :price, :phone_number, :picture_url
 
 
 end
