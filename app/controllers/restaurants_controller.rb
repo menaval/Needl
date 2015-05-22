b30cfe1f761a96ae8eea6306dce4d101c97b8234
Valentin Menard <valentin.menard@essec.edu>
2015-05-22 17:01:36 +0200

    plusieurs photos en attendant le carousel

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @picture = @restaurant.restaurant_pictures.first ? @restaurant.restaurant_pictures.first.picture : @restaurant.picture_url
+    @pictures = @restaurant.restaurant_pictures.first ? @restaurant.restaurant_pictures.map {|element| element.picture} : [@restaurant.picture_url]
   end
 
   def index
