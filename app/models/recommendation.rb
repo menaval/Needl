ac3360cef038c5a555bbd7f493335c67f221d78e
Valentin Menard <valentin.menard@essec.edu>
2015-05-27 17:04:21 +0200

    plus besoin de renseigner le prix que pour le premier

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,10 +2,8 @@ class Recommendation < ActiveRecord::Base
   include PublicActivity::Model
   belongs_to :user
   belongs_to :restaurant
-  validates :price,     presence: true
   validates :strengths, presence: true
   validates :ambiences, presence: true
-  # checker commen
   has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
   tracked owner: Proc.new { |controller, _model| controller.current_user }, only: [:create, :update]
 end
