77d9a8899eb058aeb1b6802b4a75bef2b719dd56
Valentin Menard <valentin.menard@essec.edu>
2015-06-08 11:00:42 +0200

    interactions with friends checked

--- app/controllers/not_interested_relations_controller.rb
+++ app/controllers/not_interested_relations_controller.rb
@@ -3,7 +3,7 @@ class NotInterestedRelationsController < ApplicationController
   def create
     @not_interested_relation = NotInterestedRelation.new(not_interested_relation_params)
     @not_interested_relation.save
-    redirect_to friendships_path
+    redirect_to new_friendship_path
   end
 
   private
