41cdecdd57dc8db72b92a7c7af731e9e090388ea
Valentin Menard <valentin.menard@essec.edu>
2015-06-12 13:09:55 +0200

    garder en memoire la syntaxe

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,7 @@ module Api
 
     def show
       # @user = User.find(params["id"].to_i)
+      # http://localhost:3000/api/users/40.json?user_email=valentin.menard@essec.edu&user_token=6Y-zoSafp5ynxURyFkMq
       @user = User.find_by(authentication_token: params["user_token"])
       @restaurants = Restaurant.joins(:recommendations).where(recommendations: { user_id: @user.id })
     end
