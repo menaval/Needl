01145442bccfc36488e02a6d77f724f3a79bac35
Valentin Menard <valentin.menard@essec.edu>
2015-06-12 17:49:08 +0200

    besoin d'etre connecte pour l'api de notifs

--- app/controllers/api/recommendations_controller.rb
+++ app/controllers/api/recommendations_controller.rb
@@ -1,5 +1,6 @@
 module Api
   class RecommendationsController < ApplicationController
+    acts_as_token_authentication_handler_for User
     skip_before_action :verify_authenticity_token
     skip_before_filter :authenticate_user!
 
