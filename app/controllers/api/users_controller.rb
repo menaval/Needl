396c788b60b311517a983ccfc7794a35efba462b
Valentin Menard <valentin.menard@essec.edu>
2015-06-17 17:39:06 +0200

    api user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,9 +5,9 @@ module Api
     skip_before_filter :authenticate_user!
 
     def show
-      # @user = User.find(params["id"].to_i)
+      @user = User.find(params["id"].to_i)
       # http://localhost:3000/api/users/40.json?user_email=valentin.menard@essec.edu&user_token=6Y-zoSafp5ynxURyFkMq
-      @user = User.find_by(authentication_token: params["user_token"])
+      # @user = User.find_by(authentication_token: params["user_token"])
       @restaurants = @user.my_restaurants
     end
 
