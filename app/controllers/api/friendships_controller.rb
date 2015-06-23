bed3f9a8904bca1af4aea8d057ce7a63962cce55
Valentin Menard <valentin.menard@essec.edu>
2015-06-23 15:24:52 +0200

    create fonction

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -17,5 +17,17 @@ module Api
       @new_potential_friends = current_user.user_friends - User.where(id: current_user.my_friends_ids) - User.where(id: current_user.my_requests_sent_ids) - User.where(id: current_user.my_requests_received_ids) - User.where(id: current_user.refused_relations_ids) - [current_user]
     end
 
+    def create
+      @friendship = Friendship.new(friendship_params)
+      @restaurant.save
+      render :new
+    end
+
+    private
+
+    def friendship_params
+      params.require(:friendship).permit(:sender_id, :receiver_id, :accepted)
+    end
+
   end
 end
