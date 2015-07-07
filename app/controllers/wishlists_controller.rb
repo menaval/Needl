a041e4c25be339616006dd54e7f6b1ca34315550
Valentin Menard <valentin.menard@essec.edu>
2015-07-07 11:35:18 +0200

    pret pour pouvoir ajouter sur la wishlist un restaurant depuis restaurant_show

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,5 +1,18 @@
 class WishlistsController < ApplicationController
 
+  def index
+    @wishlists = Wishlist.all
+  end
 
+  def create
+    @wishlist = Wishlist.new(wishlist_params)
+    render :new, notice: "Restaurant ajouté à ta wishlist"
+  end
+
+  private
+
+  def wishlist_params
+    params.require(:wishlist).permit(:user_id, :restaurant_id)
+  end
 
 end
