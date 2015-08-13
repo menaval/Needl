4127979bcf35e6a3ccfdf6034b93aeba02a65688
Valentin Menard <valentin.menard@essec.edu>
2015-08-13 15:47:42 +0200

    hec_2015 access limite aux entrepreneurs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def verification_code
     code = params[:verification][:code]
-    if code == "friend" || code == "guest"
+    if code == "friend" || code == "guest" || code == "hec_2015"
       redirect_to new_recommendation_path, notice: "Partage ta première reco avant de découvrir celles de tes amis !"
     else
       redirect_to access_users_path, notice: "Le code n'est pas valide"
