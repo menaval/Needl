457faf49d0dc6cf17d321a2a8a47c69ede1ef150
Valentin Menard <valentin.menard@essec.edu>
2015-08-13 13:00:50 +0200

    retirer wishes des notifs

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,7 +1,7 @@
 class Wish < ActiveRecord::Base
-  include PublicActivity::Model
+  # include PublicActivity::Model
   belongs_to :user
   belongs_to :restaurant
-  has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
-  tracked owner: Proc.new { |controller, _model| controller.current_user }, only: [:create]
+  # has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
+  # tracked owner: Proc.new { |controller, _model| controller.current_user }, only: [:create]
 end
