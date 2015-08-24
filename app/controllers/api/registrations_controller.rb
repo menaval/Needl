19f48d1cd5846f7fbc904c460a53d71983d143c4
Valentin Menard <valentin.menard@essec.edu>
2015-08-24 11:18:35 +0200

    registrations redirection

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -5,15 +5,20 @@ module Api
     skip_before_filter :authenticate_user!
 
     def edit
-      @user = User.find_by(authentication_token: params["user_token"])
+      # @user = User.find_by(authentication_token: params["user_token"])
+      @user = User.find(params['id'])
       if params['email']
+        @name = params['name']
+        @email = params['email']
         update
       end
     end
 
     def update
-      @user.update_attributes(name: params['name'], email: params['email'])
-      redirect_to edit_api_registration_path
+
+      @user.update_attributes(name: @name, email: @email)
+      redirect_to edit_api_registration_path(:user_email => params["user_email"], :user_token => params["user_token"])
+
     end
   end
 end
