47aa5de352fc787036373c7edef715d43b7e0ed8
Valentin Menard <valentin.menard@essec.edu>
2015-10-20 16:52:10 +0200

    envoyer notifs lors d'un nouveau post d'expert

--- app/admin/recommendation.rb
+++ app/admin/recommendation.rb
@@ -5,10 +5,23 @@ ActiveAdmin.register Recommendation do
   batch_action :reco_from_expert do |ids|
     Recommendation.find(ids).each do |recommendation|
       activity = PublicActivity::Activity.find_by(trackable_type: "Recommendation", trackable_id: recommendation.id)
-      activity.owner_id = recommendation.expert_id
+      expert = Expert.find(recommendation.expert_id)
+      restaurant = Restaurant.find(recommendation.restaurant_id)
+      activity.owner_id = expert.id
       activity.owner_type = "Expert"
       activity.save
-    end
+
+      #  envoie des notifs
+
+    client = Parse.create(application_id: ENV['PARSE_APPLICATION_ID'], api_key: ENV['PARSE_API_KEY'], master_key:ENV['PARSE_MASTER_KEY'])
+      # envoyer à tous les followers que l'expert a fait une nouvelle reco du resto @restaurant
+      data = { :alert => "#{expert.name} a recommande #{restaurant.name}", :badge => 'Increment', :type => 'reco'  }
+      push = client.push(data)
+      # push.type = "ios"
+      query = client.query(Parse::Protocol::CLASS_INSTALLATION).value_in('user_id', expert.followers.pluck(:id))
+      push.where = query.where
+      push.save
+
   redirect_to admin_recommendations_path, alert: "Expertise tamponnée !"
   end
 
