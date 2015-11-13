f548ce732b65de16cf09f3f5027f8759d6a5f5a4
Valentin Menard <valentin.menard@essec.edu>
2015-11-13 19:32:36 +0100

    test ajouté users base mailchimp 2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,24 +15,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       else
         @tracker.track(current_user.id, 'signin', {"user" => user.name, "browser" => browser.name} )
 
-        #  test a retirer
-        @gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-        @list_id = ENV['MAILCHIMP_LIST_ID_NEEDL_USERS']
-        User.all.each do |user|
-          @gibbon.lists(@list_id).members.create(
-            body: {
-              email_address: user.email,
-              status: "subscribed",
-              merge_fields: {
-                FNAME: user.name.partition(" ").first,
-                LNAME: user.name.partition(" ").last
-              }
-            }
-          )
-        end
-
-        # fin du test
-
         redirect_to root_path
       end
 
