d776d54085a3b846cc4ce4e54cc99d5b893f56ee
Valentin Menard <valentin.menard@essec.edu>
2015-11-28 19:14:22 +0100

    test skip before filter authentication pour le bug de connexion fb

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -2,7 +2,7 @@ module Api
   class RegistrationsController < ApplicationController
     # acts_as_token_authentication_handler_for User
     skip_before_action :verify_authenticity_token
-    skip_before_filter :authenticate_user!
+
 
     def edit
       # @user = User.find_by(authentication_token: params["user_token"])
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_filter :authenticate_user!
+
   def facebook
     user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
     if user.token_expiry < Time.now
