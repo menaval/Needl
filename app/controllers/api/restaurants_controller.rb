6cafece32aa676b35a9ae8991164bd73b1fdc57c
Valentin Menard <valentin.menard@essec.edu>
2015-12-06 11:30:09 +0100

    enleve les occasions pour l'instant

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -38,13 +38,13 @@ module Api
 
       # associer les ambiances, occasions et amis recommandant aux restaurants avec une seule requête
       @all_ambiences = {}
-      @all_occasions = {}
+      # @all_occasions = {}
       @all_friends_recommending = {}
       @recommendations_from_friends.each do |recommendation|
         @all_ambiences[recommendation.restaurant_id] ||= []
         @all_ambiences[recommendation.restaurant_id] << recommendation.ambiences
-        @all_occasions[recommendation.restaurant_id] ||= []
-        @all_occasions[recommendation.restaurant_id] << recommendation.occasions
+        # @all_occasions[recommendation.restaurant_id] ||= []
+        # @all_occasions[recommendation.restaurant_id] << recommendation.occasions
         @all_friends_recommending[recommendation.restaurant_id] ||= []
         @all_friends_recommending[recommendation.restaurant_id] << recommendation.user_id
       end
