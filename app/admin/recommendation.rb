5b9008de7e76694363a921de93bcafca49d5df7b
Valentin Menard <valentin.menard@essec.edu>
2015-12-14 09:09:32 +0100

    bug admin resolu

--- app/admin/recommendation.rb
+++ app/admin/recommendation.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Recommendation do
 
-  permit_params :ambiences, :strengths, :review, :restaurant_id, :user_id, :occasions
+  permit_params :ambiences, :strengths, :review, :restaurant_id, :user_id, :occasions, :price_ranges
 
 end
