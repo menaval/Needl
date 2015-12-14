f645e67da32eb511a6c7f734fe7db6802c29d01f
Valentin Menard <valentin.menard@essec.edu>
2015-12-14 09:34:14 +0100

    amelioration interface admin

--- app/admin/recommendation.rb
+++ app/admin/recommendation.rb
@@ -2,4 +2,16 @@ ActiveAdmin.register Recommendation do
 
   permit_params :ambiences, :strengths, :review, :restaurant_id, :user_id, :occasions, :price_ranges
 
+  form do |f|
+    f.inputs "Recommendation" do
+      f.input :restaurant, collection: Restaurant.all.order(:name)
+      f.input :user, collection: User.all.order(:name)
+      f.input :ambiences
+      f.input :strengths
+      f.input :occasions
+      f.input :review
+    end
+    f.actions
+  end
+
 end
--- app/admin/wish.rb
+++ app/admin/wish.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register Wish do
 
   permit_params :restaurant_id, :user_id
+
+  form do |f|
+    f.inputs "Wish" do
+      f.input :restaurant, collection: Restaurant.all.order(:name)
+      f.input :user, collection: User.all.order(:name)
+    end
+    f.actions
+  end
+
   batch_action :wish_from_admin do |ids|
     Wish.find(ids).each do |wish|
       activity = PublicActivity::Activity.find_by(trackable_type: "Wish", trackable_id: wish.id)
