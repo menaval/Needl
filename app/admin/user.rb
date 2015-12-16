638365f18a44bcaa62bfcea7fcc29289a4183401
Valentin Menard <valentin.menard@essec.edu>
2015-12-16 14:38:41 +0100

    numero de tels sur admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,12 +1,13 @@
 ActiveAdmin.register User do
 
-  permit_params :name, :email, :password, :admin, :picture, :emails
+  permit_params :name, :email, :password, :admin, :picture, :emails, :phone_numbers
 
   form do |f|
     f.inputs "Identity" do
       f.input :name
       f.input :email
       f.input :emails
+      f.input :phone_numbers
       f.file_field :picture
     end
     f.inputs "Admin" do
