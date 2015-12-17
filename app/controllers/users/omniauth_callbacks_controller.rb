8c198390ca50cd497b0a6082e54620936b6b2a25
Valentin Menard <valentin.menard@essec.edu>
2015-12-17 09:14:04 +0100

    test en remettant mailchimp

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -56,18 +56,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
         @list_id = ENV['MAILCHIMP_LIST_ID_NEEDL_USERS']
 
-        # @gibbon.lists(@list_id).members.create(
-        #   body: {
-        #     email_address: @user.email,
-        #     status: "subscribed",
-        #     merge_fields: {
-        #       FNAME: @user.name.partition(" ").first,
-        #       LNAME: @user.name.partition(" ").last,
-        #       TOKEN: @user.authentication_token,
-        #       GENDER: @user.gender
-        #     }
-        #   }
-        # )
+        @gibbon.lists(@list_id).members.create(
+          body: {
+            email_address: @user.email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: @user.name.partition(" ").first,
+              LNAME: @user.name.partition(" ").last,
+              TOKEN: @user.authentication_token,
+              GENDER: @user.gender
+            }
+          }
+        )
 
         accept_all_friends
 
