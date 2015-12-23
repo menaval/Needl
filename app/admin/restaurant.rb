dbe7af8f7810a875f29b0876d4de25243a7a6da7
Valentin Menard <valentin.menard@essec.edu>
2015-12-23 18:29:22 +0100

    admin simplifié pour edit un resto

--- app/admin/restaurant.rb
+++ app/admin/restaurant.rb
@@ -2,6 +2,49 @@ ActiveAdmin.register Restaurant do
 
   permit_params :name, :address, :food_id, :longitude, :latitude, :phone_number, :picture_url, :price_range, :city, :postal_code, :full_address, :starter1, :starter2, :price_starter1, :price_starter2, :main_course1, :main_course2, :main_course3, :price_main_course1, :price_main_course2, :price_main_course3, :dessert1, :dessert2, :price_dessert1, :price_dessert2, :description_starter1, :description_starter2, :description_main_course1, :description_main_course2, :description_main_course3, :description_dessert1, :description_dessert2, :food_name, :subway_name, :subways_near, :needl_coefficient
 
+  form do |f|
+    f.inputs "Restaurant" do
+      f.input :food, collection: Food.all.order(:name)
+      f.input :name
+      f.input :address
+      f.input :city
+      f.input :postal_code
+      f.input :full_address
+      f.input :price_range
+      f.input :phone_number
+      f.input :starter_1
+      f.input :description_starter1
+      f.input :price_starter1
+      f.input :starter_2
+      f.input :description_starter2
+      f.input :price_starter2
+      f.input :main_course1
+      f.input :description_main_course1
+      f.input :price_main_course1
+      f.input :main_course2
+      f.input :description_main_course2
+      f.input :price_main_course2
+      f.input :main_course3
+      f.input :description_main_course3
+      f.input :price_main_course3
+      f.input :dessert1
+      f.input :description_dessert1
+      f.input :price_dessert1
+      f.input :dessert2
+      f.input :description_dessert2
+      f.input :price_dessert2
+      f.input :latitude
+      f.input :longitude
+      f.input :picture_url
+      f.input :food_name
+      f.input :subway_name
+      f.input :subways_near
+      f.input :needl_coefficient
+      f.file_field :picture
+    end
+    f.actions
+  end
+
   batch_action :new_restaurant do |ids|
     Restaurant.find(ids).each do |restaurant|
       # ajouter le food name
