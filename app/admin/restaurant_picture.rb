8478b0cc83e6b02f04ba7b9ae1b07cbed4bcd9f8
Valentin Menard <valentin.menard@essec.edu>
2015-12-23 14:47:08 +0100

    classer active admin

--- app/admin/restaurant_picture.rb
+++ app/admin/restaurant_picture.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register RestaurantPicture do
   permit_params :picture, :restaurant_id
 
+  filter :restaurant, collection: Restaurant.all.order(:name)
+
   form do |f|
     f.inputs "Picture" do
       f.input :restaurant, collection: Restaurant.all.order(:name)
