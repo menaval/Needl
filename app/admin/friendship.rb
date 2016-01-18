1c58f7df2dcd8c156381bd2584c77752f8dca41e
Valentin Menard <valentin.menard@essec.edu>
2016-01-18 11:56:20 +0100

    custom admin

--- app/admin/friendship.rb
+++ app/admin/friendship.rb
@@ -2,5 +2,7 @@ ActiveAdmin.register Friendship do
 
   permit_params :sender_id, :receiver_id, :accepted, :sender_invisible, :receiver_invisible
 
+  filter :sender, collection: User.all.order(:name)
+  filter :receiver, collection: User.all.order(:name)
 
 end
