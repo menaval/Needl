3a8415c5e7545c8cd8902c914b24789fe01cb869
Valentin Menard <valentin.menard@essec.edu>
2016-01-29 00:21:36 +0100

    test bug active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,20 +2,20 @@ ActiveAdmin.register User do
 
   permit_params :name, :email, :password, :admin, :picture, :emails, :phone_numbers, :score
 
-  form do |f|
-    f.inputs "Identity" do
-      f.input :name
-      f.input :email
-      f.input :emails
-      f.input :phone_numbers
-      f.input :score
-      f.file_field :picture
-    end
-    f.inputs "Admin" do
-      f.input :admin
-    end
-    f.actions
-  end
+  # form do |f|
+  #   f.inputs "Identity" do
+  #     f.input :name
+  #     f.input :email
+  #     f.input :emails
+  #     f.input :phone_numbers
+  #     f.input :score
+  #     f.file_field :picture
+  #   end
+  #   f.inputs "Admin" do
+  #     f.input :admin
+  #   end
+  #   f.actions
+  # end
 
 end
 
