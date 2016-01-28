4ba8f49a431772ca5a8e17e86bee39dac4e1a4fe
Valentin Menard <valentin.menard@essec.edu>
2016-01-29 00:25:24 +0100

    test bug active admin 3

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,16 +4,16 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs "Identity" do
-      # f.input :name
-      # f.input :email
-      # f.input :emails
-      # f.input :phone_numbers
-      # f.input :score
+      f.input :name
+      f.input :email
+      f.input :emails
+      f.input :phone_numbers
+      f.input :score
       f.file_field :picture
     end
-    # f.inputs "Admin" do
-    #   f.input :admin
-    # end
+    f.inputs "Admin" do
+      f.input :admin
+    end
     f.actions
   end
 
