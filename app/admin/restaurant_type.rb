a433e39ddb76eb448a3f969744f418d15163c85b
Valentin Menard <valentin.menard@essec.edu>
2016-02-08 17:32:05 +0100

    admin simplifie pour creer un restaurant type

--- app/admin/restaurant_type.rb
+++ app/admin/restaurant_type.rb
@@ -2,4 +2,12 @@ ActiveAdmin.register RestaurantType do
 
   permit_params :restaurant_id, :type_id
 
+  form do |f|
+    f.inputs "Restaurant Type" do
+      f.input :restaurant, collection: Restaurant.all.order(:name)
+      f.input :type, collection: Type.all.order(:name)
+    end
+    f.actions
+  end
+
 end
