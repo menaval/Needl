87e4fa644479bfa72fd62896898bb9a378eddf6d
Valentin Menard <valentin.menard@essec.edu>
2016-02-29 17:05:15 +0100

    test 4 signin

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,11 +5,23 @@ module Api
     skip_before_filter :authenticate_user!
 
     def create
-      resource = warden.authenticate!(:scope => :user)
-      sign_in(:user, resource)
-      puts "#{resource.name}"
-      puts "#{resource.email}"
-      render json: {user: resource, nb_recos: Restaurant.joins(:recommendations).where(recommendations: { user_id: resource.id }).count, nb_wishes: Restaurant.joins(:wishes).where(wishes: {user_id: resource.id}).count}
+      email = params['email']
+      password = params['password']
+      @user = User.find_by(email: 'yolo@gmail.co')
+
+      # connection réussie
+      if @user != nil && password == @user.encrypted_password
+        sign_in @user
+        render json: {user: @user, nb_recos: Restaurant.joins(:recommendations).where(recommendations: { user_id: @user.id }).count, nb_wishes: Restaurant.joins(:wishes).where(wishes: {user_id: @user.id}).count}
+      # l'utilisateur a rempli la bonne adresse mail mais il s'est inscrit via facebook
+      elsif @user != nil && @user.token != nil
+        render json: {error_message: "ce compte est relié à une connection par Facebook !"}
+      #  l'utilisateur a rempli la bonne adresse mais mauvais mot de passe
+      elsif @user != nil && password != @user.encrypted_password
+        render json: {error_message: "Tu as la bonne adresse mail mais le mot de passe n'est pas le bon"}
+      else
+        render json: {error_message: "Nous n'avons aucun compte à cette adresse mail"}
+      end
 
       # la requete postman
       # http://localhost:3000/api/sessions.json?user[email]=yolo4@gmail.co&user[password]=12345678
