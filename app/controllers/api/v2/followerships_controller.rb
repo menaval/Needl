4a716b803544e34e04f51c667d845d6eec0155f1
Valentin Menard <valentin.menard@essec.edu>
2016-03-09 17:28:42 +0100

    bug 11 fixed

--- app/controllers/api/v2/followerships_controller.rb
+++ app/controllers/api/v2/followerships_controller.rb
@@ -78,7 +78,7 @@ class Api::V2::FollowershipsController < ApplicationController
     end
 
     followership.destroy
-    @tracker.track(user.id, 'Followership Destroyed', { "user" => @user.name, "following" => following.name})
+    @tracker.track(@user.id, 'Followership Destroyed', { "user" => @user.name, "following" => following.name})
 
     # renvoyer restaurants
     redirect_to user_updated_api_v2_restaurants_path(:user_id => following.id, :user_email => params["user_email"], :user_token => params["user_token"])
