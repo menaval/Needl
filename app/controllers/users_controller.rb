887f1002bc56223d70d17973c3d4e80aceb2121c
Valentin Menard <valentin.menard@essec.edu>
2016-03-17 10:47:45 +0100

    mise en place formulaire mot de passe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,22 @@ class UsersController < ApplicationController
 
   end
 
-  # def change_password
-  #   @user = User.find_by(authentication_token: params["user_token"])
-  # end
+  def change_password
+    @user = User.find_by(authentication_token: params["user_token"])
+  end
 
-  # def update_password
-  #   @user = User.find_by(authentication_token: params["user_token"])
-  #   password = params["password"]
-  #   password_confirmation = params["password_confirmation"]
-  #   @user.update_attributes(password: password)
-  #   if pass
-  #   render(:json => {notice: "Ton mot de passe a bien été changé !"}, :status => 409, :layout => false)
-  # end
+  def update_password
+    @user = User.find_by(authentication_token: params["user_token"])
+    password = params["password"]
+    password_confirmation = params["password_confirmation"]
+    if @user.token
+      render(:json => {notice: "Nous ne pouvons pas changer ton mot de passe, tu t'es inscrit via facebook !"}, :status => 409, :layout => false)
+    elsif password != password_confirmation
+      render(:json => {notice: "Erreur: Les deux mots de passe étaient différents."}, :status => 409, :layout => false)
+    else
+    @user.update_attributes(password: password)
+    render(:json => {notice: "Ton mot de passe a bien été changé !"}, :status => 409, :layout => false)
+    end
+  end
 
 end
