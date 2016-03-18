4e86dcc4efdd68536a713808704d0ef1af26f4ba
Valentin Menard <valentin.menard@essec.edu>
2016-03-18 11:43:29 +0100

    test update password 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.find_by(authentication_token: params["user_token"])
     password = params["password"]
     password_confirmation = params["password_confirmation"]
-    if @user.token
+    if @user.provider == "facebook"
       render(:json => {notice: "Nous ne pouvons pas changer ton mot de passe, tu t'es inscrit via facebook !"}, :status => 409, :layout => false)
     elsif password != password_confirmation
       render(:json => {notice: "Erreur: Les deux mots de passe étaient différents."}, :status => 409, :layout => false)
