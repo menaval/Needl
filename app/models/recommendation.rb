f4544c352fe4741efd3a1ea0b781a46f4c9b25fb
Valentin Menard <valentin.menard@essec.edu>
2016-03-18 19:02:15 +0100

    enlever les obligations de la reco

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,11 +2,6 @@ class Recommendation < ActiveRecord::Base
   include PublicActivity::Model
   belongs_to :user
   belongs_to :restaurant
-  validates :strengths, presence: true
-  validates :ambiences, presence: true
-
-  # On garde pour l'instant sans les occasions pour l'ancienne version
-  # validates :occasions, presence: true
   attr_accessor :wish
   has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
   tracked owner: Proc.new { |controller, _model| controller.current_user }, only: [:create]
