6d902e3216c170ad7bba343c00af1d7b91bf5168
Valentin Menard <valentin.menard@essec.edu>
2016-03-24 14:37:46 +0100

    admin added

--- app/admin/restaurant_picture.rb
+++ app/admin/restaurant_picture.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register RestaurantPicture do
-  permit_params :picture, :restaurant_id
+  permit_params :picture, :restaurant_id, :user_id
 
   filter :restaurant, collection: Restaurant.all.order(:name)
 
   form do |f|
     f.inputs "Picture" do
       f.input :restaurant, collection: Restaurant.all.order(:name)
+      f.input :user, collection: User.where(public: true).order(:name)
       f.file_field :picture
     end
     f.actions
